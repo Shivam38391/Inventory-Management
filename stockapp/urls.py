b4a0d60d27b3ce4451414074329a0a01bb9ae716
shivam sharma <shivamsharma38391@gmail.com>
2023-05-05 01:35:51 +0530

    talbles

--- stockapp/urls.py
+++ stockapp/urls.py
@@ -7,4 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
  path("add-stock", views.add_stock, name="add-stock"),
+ path("list-stock", views.add_stock, name="add-stock"),
+  path('my-view/', views.ListView.as_view(), name='my_view')
+ 
 ] 
--- stockapp/views.py
+++ stockapp/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
+from django.views import View
 
+from stockapp.models import Stock
+from .forms import StockForm
 # Create your views here.
 
 def add_stock(request):
+    
+    # if request.method == 'POST':
+        
+    
     return render(request, "stockapp/add_stock.html")
+
+
+def list_stock(request):
+    
+    stocks = Stock.objects.all()
+    
+    context = {
+        "stocks":stocks,
+    }
+    
+    return render(request, "stockapp/list_stock.html", context)
+
+
+
+
+class ListView(View):
+    template_name= "stockapp/stocktable.html"
+    
+    model = Stock
+    def get(self, request, *args, **kwargs):
+        
+        stocks = self.model.objects.all()
+        
+        context = {
+            "stocks": stocks,
+        }
+        return render(request,self.template_name, context)
